abe773ba564e1fd6cddb6966206446fe1b141901
SergioFC1989 <sergiofc@hotmail.es>
2021-09-26 14:34:19 +0200

    Integración de Firebase

--- src/index.js
+++ src/index.js
@@ -2,23 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import reportWebVitals from './reportWebVitals';
-import { initializeApp } from "firebase/app";
 import { Grommet } from 'grommet';
 import styled from 'styled-components';
 import theme from './theme'
 import App from './App';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBuOOojJIwC_wjhCWQX4ZhoHRJ7QU5-Pjg",
-  authDomain: "abfe-5cd3a.firebaseapp.com",
-  projectId: "abfe-5cd3a",
-  storageBucket: "abfe-5cd3a.appspot.com",
-  messagingSenderId: "252087285346",
-  appId: "1:252087285346:web:cb2fca422446216ece833b"
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
 const StyledGrommet = styled(Grommet)`
   height: 100%;
